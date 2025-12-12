6ee0483ef69d3acbbacba1f81182e246a8f8c177
Gravady <markar6@kfvelev.se>
2025-12-12 13:03:04 +0100

    Ändring

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 import datetime
 import time
 
@@ -6,26 +6,32 @@ import file
 import client
 import temp
 
-
 def main():
     print("Program startup")
     print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     start_time = time.time()
-    try : 
-        client = client.SocketConnection()
-        client.setJsonPreffered(True)
-        temp = client.getData()
-        temp = temp.TEMP_STAT()
-        client.sendDataEntry(temp)
-    except KeyboardInterrupt :
+
+    try:
+        client_instance = client.SocketConnection()
+        client_instance.setJsonPreffered(True)
+
+        temp_data = client_instance.getData()
+        temp_stat = temp.TEMP_STAT()
+
+        client_instance.sendDataEntry(temp_stat)
+
+    except KeyboardInterrupt:
+        print("\nProgram interrupted by user.")
+    finally:
+        if 'client_instance' in locals():
+            if hasattr(client_instance, 'close'):
+                client_instance.close()
+            else:
+                del client_instance
+
         print("Program shutdown")
         print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         print("--- %s seconds ---" % (time.time() - start_time), "Runtime")
-        sys.exit(0)
-    finally : 
-        client.__del__()
-    
-    print("Program shutdown")
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    print("--- %s seconds ---" % (time.time() - start_time), "Runtime")
 
+if __name__ == "__main__":
+    main()
